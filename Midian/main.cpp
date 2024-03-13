64a1c7f939e3d109e3dda740b3233f17188b645a
saifahmed34 <114881135+saifahmed34@users.noreply.github.com>
2024-03-13 21:31:21 +0200

    Update main.cpp

--- Midian/main.cpp
+++ Midian/main.cpp
@@ -10,15 +10,43 @@ int main() {
         cin >> a[i];
     }
     sort(a.begin(), a.end());
+
     if (n % 2 != 0) {
         // Median for odd number of elements
-        int medianIndex = n / 2;
-        cout << a[medianIndex];
+        int low = 0;
+        int high = a.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] < a[a.size() / 2])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        cout << a[low]; // Output median for odd number of elements
     } else {
         // Median for even number of elements
-        int medianIndex1 = n / 2 - 1;
-        int medianIndex2 = n / 2;
-        cout << (a[medianIndex1] + a[medianIndex2]) / 2.0; // Average of middle two elements
+        int low = 0;
+        int high = a.size() - 1;
+        int median1, median2;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] < a.size() / 2)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        median1 = a[low];
+        low = 0;
+        high = a.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] < a.size() / 2 + 1)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        median2 = a[low];
+        cout << (median1 + median2) / 2.0; // Output median for even number of elements
     }
 
     return 0;
